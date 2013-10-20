b39bf2112906d02391ea7225f6f271b93480854b
Magnus Achim Deininger <magnus@ef.gy>
2013-10-20 17:37:56 +0000

    cleanups to the IFS code; push the sierpinski gasket and carpet transformations into the common ifs base template

--- include/ef.gy/ifs.h
+++ include/ef.gy/ifs.h
@@ -30,12 +30,16 @@
 #define EF_GY_IFS_H
 
 #include <ef.gy/polytope.h>
+#include <ef.gy/projection.h>
+#include <vector>
 
 namespace efgy
 {
     namespace geometry
     {
-        template <typename Q, unsigned int od, typename render, unsigned int d = od>
+        template <typename Q, unsigned int od, typename render, unsigned int d,
+                  template <class,unsigned int,class,unsigned int> class primitive,
+                  template <class,unsigned int> class transformation>
         class ifs : public polytope<Q,d,4,render>
         {
             public:
@@ -56,32 +60,99 @@ namespace efgy
                 using parent::modelDimensionMaximum;
                 using parent::renderDimensionMinimum;
                 using parent::renderDimensionMaximum;
+
+                std::vector<transformation<Q,d> > functions;
+
+                void calculateObject (void)
+                {
+                    primitive<Q,od,render,d> source(parent::renderer, parameter, parent::precisionMultiplier);
+                    
+                    lines = source.lines;
+                    faces = source.faces;
+                    
+                    for (unsigned int i = 0; i < parameter.iterations; i++)
+                    {
+                        decltype(lines) rlines;
+                        decltype(faces) rfaces;
+                        
+                        while (lines.size() > 0)
+                        {
+                            for (unsigned int j = 0; j < functions.size(); j++)
+                            {
+                                rlines.push_back(apply(j,lines.back()));
+                            }
+                            lines.pop_back();
+                        }
+                        
+                        while (faces.size() > 0)
+                        {
+                            for (unsigned int j = 0; j < functions.size(); j++)
+                            {
+                                rfaces.push_back(apply(j,faces.back()));
+                            }
+                            faces.pop_back();
+                        }
+                        
+                        lines = rlines;
+                        faces = rfaces;
+                    }
+                }
+            
+                typename euclidian::space<Q,d>::vector apply
+                    (const unsigned int &f,
+                     const typename euclidian::space<Q,d>::vector &v)
+                {
+                    return functions[f] * v;
+                }
+            
+                template<unsigned int fdim>
+                math::tuple<fdim,typename euclidian::space<Q,d>::vector> apply
+                    (const unsigned int &f,
+                     const math::tuple<fdim,typename euclidian::space<Q,d>::vector> &l)
+                {
+                    math::tuple<fdim,typename euclidian::space<Q,d>::vector> r;
+                    
+                    for (int i = 0; i < fdim; i++)
+                    {
+                        r.data[i] = apply (f, l.data[i]);
+                    }
+                    
+                    return r;
+                }
         };
 
         namespace sierpinski
         {
             template <typename Q, unsigned int od, typename render, unsigned int d = od>
-            class gasket : public ifs<Q,od,render,d>
+            class gasket : public ifs<Q,od,render,d,cube,transformation>
             {
                 public:
-                    typedef ifs<Q,od,render,d> parent;
+                    typedef ifs<Q,od,render,d,cube,transformation> parent;
 
                     gasket(render &pRenderer, const parameters<Q> &pParameter, const Q &pMultiplier = 1)
                         : parent(pRenderer, pParameter, pMultiplier)
                         {
-                            translation.data[0].data[0] = Q(0.25);
+                            const unsigned int nfunctions = (1<<(od-1))+1;
+                            math::tuple<nfunctions, typename euclidian::space<Q,d>::vector> translations;
 
-                            for (unsigned int i = 1; i < functions; i++)
+                            translations.data[0].data[0] = Q(0.25);
+
+                            for (unsigned int i = 1; i < nfunctions; i++)
                             {
-                                translation.data[i].data[0] = Q(-0.25);
+                                translations.data[i].data[0] = Q(-0.25);
                                 for (unsigned int j = 1; j < od; j++)
                                 {
                                     const unsigned int k = i-1;
                                     const unsigned int l = j-1;
                                     const unsigned int b = 1 << l;
                                     const bool s = k & b;
-                                    translation.data[i].data[j] = Q(s ? -0.25 : 0.25);
+                                    translations.data[i].data[j] = Q(s ? -0.25 : 0.25);
                                 }
+                            }
+
+                            for (unsigned int i = 0; i < nfunctions; i++)
+                            {
+                                functions.push_back(scale<Q,d>(0.5) * translation<Q,d>(translations.data[i]));
                             }
 
                             calculateObject();
@@ -99,123 +170,70 @@ namespace efgy
                     using parent::renderDimensionMinimum;
                     using parent::renderDimensionMaximum;
 
+                    using parent::functions;
+                    using parent::calculateObject;
+
                     static unsigned int depth (void) { return od; }
                     static unsigned int renderDepth (void) { return d; }
                     static const char *id (void) { return "sierpinski-gasket"; }
-
-                    static const unsigned int functions = (1<<(od-1))+1;
-                    math::tuple<functions, typename euclidian::space<Q,d>::vector> translation;
-
-                    void calculateObject (void)
-                    {
-                        cube<Q,od,render,d> source(parent::renderer, parameter, parent::precisionMultiplier);
-
-                        lines = source.lines;
-                        faces = source.faces;
-
-                        for (unsigned int i = 0; i < parameter.iterations; i++)
-                        {
-                            decltype(lines) rlines;
-                            decltype(faces) rfaces;
-
-                            while (lines.size() > 0)
-                            {
-                                for (unsigned int j = 0; j < functions; j++)
-                                {
-                                    rlines.push_back(apply(j,lines.back()));
-                                }
-                                lines.pop_back();
-                            }
-
-                            while (faces.size() > 0)
-                            {
-                                for (unsigned int j = 0; j < functions; j++)
-                                {
-                                    rfaces.push_back(apply(j,faces.back()));
-                                }
-                                faces.pop_back();
-                            }
-
-                            lines = rlines;
-                            faces = rfaces;
-                        }
-                    }
-
-                    typename euclidian::space<Q,d>::vector apply
-                        (const unsigned int &f,
-                         const typename euclidian::space<Q,d>::vector &v)
-                    {
-                        typename euclidian::space<Q,d>::vector r = v * Q(0.5);
-
-                        r = r + translation.data[f];
-
-                        return r;
-                    }
-
-                    template<unsigned int fdim>
-                    math::tuple<fdim,typename euclidian::space<Q,d>::vector> apply
-                        (const unsigned int &f,
-                         const math::tuple<fdim,typename euclidian::space<Q,d>::vector> &l)
-                    {
-                        math::tuple<fdim,typename euclidian::space<Q,d>::vector> r;
-
-                        for (int i = 0; i < fdim; i++)
-                        {
-                            r.data[i] = apply (f, l.data[i]);
-                        }
-
-                        return r;
-                    }
             };
 
             template <typename Q, unsigned int od, typename render, unsigned int d = od>
-            class carpet : public ifs<Q,od,render,d>
+            class carpet : public ifs<Q,od,render,d,cube,transformation>
             {
                 public:
-                    typedef ifs<Q,od,render,d> parent;
+                    typedef ifs<Q,od,render,d,cube,transformation> parent;
 
                     carpet(render &pRenderer, const parameters<Q> &pParameter, const Q &pMultiplier = 1)
                         : parent(pRenderer, pParameter, pMultiplier)
                         {
+                            const unsigned int nfunctions = od == 2 ? 8 : 20;
+                            math::tuple<nfunctions, typename euclidian::space<Q,d>::vector> translations;
+
                             if (od > 1)
                             {
-                                translation.data[0].data[0] = Q(-1)/Q(3);
-                                translation.data[0].data[1] = Q(-1)/Q(3);
-                                translation.data[1].data[0] = Q(-1)/Q(3);
-                                translation.data[1].data[1] = Q(0);
-                                translation.data[2].data[0] = Q(-1)/Q(3);
-                                translation.data[2].data[1] = Q(1)/Q(3);
-                                translation.data[3].data[0] = Q(1)/Q(3);
-                                translation.data[3].data[1] = Q(-1)/Q(3);
-                                translation.data[4].data[0] = Q(1)/Q(3);
-                                translation.data[4].data[1] = Q(0);
-                                translation.data[5].data[0] = Q(1)/Q(3);
-                                translation.data[5].data[1] = Q(1)/Q(3);
-                                translation.data[6].data[0] = Q(0);
-                                translation.data[6].data[1] = Q(-1)/Q(3);
-                                translation.data[7].data[0] = Q(0);
-                                translation.data[7].data[1] = Q(1)/Q(3);
+                                translations.data[0].data[0] = Q(-1)/Q(3);
+                                translations.data[0].data[1] = Q(-1)/Q(3);
+                                translations.data[1].data[0] = Q(-1)/Q(3);
+                                translations.data[1].data[1] = Q(0);
+                                translations.data[2].data[0] = Q(-1)/Q(3);
+                                translations.data[2].data[1] = Q(1)/Q(3);
+                                translations.data[3].data[0] = Q(1)/Q(3);
+                                translations.data[3].data[1] = Q(-1)/Q(3);
+                                translations.data[4].data[0] = Q(1)/Q(3);
+                                translations.data[4].data[1] = Q(0);
+                                translations.data[5].data[0] = Q(1)/Q(3);
+                                translations.data[5].data[1] = Q(1)/Q(3);
+                                translations.data[6].data[0] = Q(0);
+                                translations.data[6].data[1] = Q(-1)/Q(3);
+                                translations.data[7].data[0] = Q(0);
+                                translations.data[7].data[1] = Q(1)/Q(3);
                             }
                             if (od > 2)
                             {
                                 for (int i = 0; i < 8; i++)
                                 {
-                                    translation.data[(i+8)] = translation.data[i];
-                                    translation.data[i].data[2] = Q(-1)/Q(3);
-                                    translation.data[(i+8)].data[2] = Q(1)/Q(3);
+                                    translations.data[(i+8)] = translations.data[i];
+                                    translations.data[i].data[2] = Q(-1)/Q(3);
+                                    translations.data[(i+8)].data[2] = Q(1)/Q(3);
                                 }
 
-                                translation.data[16].data[0] = Q(1)/Q(3);
-                                translation.data[16].data[1] = Q(1)/Q(3);
+                                translations.data[16].data[0] = Q(1)/Q(3);
+                                translations.data[16].data[1] = Q(1)/Q(3);
 
-                                translation.data[17].data[0] = Q(-1)/Q(3);
-                                translation.data[17].data[1] = Q(1)/Q(3);
+                                translations.data[17].data[0] = Q(-1)/Q(3);
+                                translations.data[17].data[1] = Q(1)/Q(3);
 
-                                translation.data[18].data[0] = Q(1)/Q(3);
-                                translation.data[18].data[1] = Q(-1)/Q(3);
+                                translations.data[18].data[0] = Q(1)/Q(3);
+                                translations.data[18].data[1] = Q(-1)/Q(3);
 
-                                translation.data[19].data[0] = Q(-1)/Q(3);
-                                translation.data[19].data[1] = Q(-1)/Q(3);
+                                translations.data[19].data[0] = Q(-1)/Q(3);
+                                translations.data[19].data[1] = Q(-1)/Q(3);
+                            }
+
+                            for (unsigned int i = 0; i < nfunctions; i++)
+                            {
+                                functions.push_back(scale<Q,d>(Q(1)/Q(3)) * translation<Q,d>(translations.data[i]));
                             }
 
                             calculateObject();
@@ -233,73 +251,12 @@ namespace efgy
                     using parent::renderDimensionMinimum;
                     using parent::renderDimensionMaximum;
 
+                    using parent::functions;
+                    using parent::calculateObject;
+
                     static unsigned int depth (void) { return od; }
                     static unsigned int renderDepth (void) { return d; }
                     static const char *id (void) { return "sierpinski-carpet"; }
-
-                    static const unsigned int functions = od == 2 ? 8 : 20;
-                    math::tuple<functions, typename euclidian::space<Q,d>::vector> translation;
-
-                    void calculateObject (void)
-                    {
-                        cube<Q,od,render,d> source(parent::renderer, parameter, parent::precisionMultiplier);
-
-                        lines = source.lines;
-                        faces = source.faces;
-
-                        for (unsigned int i = 0; i < parameter.iterations; i++)
-                        {
-                            decltype(lines) rlines;
-                            decltype(faces) rfaces;
-
-                            while (lines.size() > 0)
-                            {
-                                for (unsigned int j = 0; j < functions; j++)
-                                {
-                                    rlines.push_back(apply(j,lines.back()));
-                                }
-                                lines.pop_back();
-                            }
-
-                            while (faces.size() > 0)
-                            {
-                                for (unsigned int j = 0; j < functions; j++)
-                                {
-                                    rfaces.push_back(apply(j,faces.back()));
-                                }
-                                faces.pop_back();
-                            }
-
-                            lines = rlines;
-                            faces = rfaces;
-                        }
-                    }
-
-                    typename euclidian::space<Q,d>::vector apply
-                        (const unsigned int &f,
-                         const typename euclidian::space<Q,d>::vector &v)
-                    {
-                        typename euclidian::space<Q,d>::vector r = v / Q(3);
-
-                        r = r + translation.data[f];
-
-                        return r;
-                    }
-
-                    template<unsigned int fdim>
-                    math::tuple<fdim,typename euclidian::space<Q,d>::vector> apply
-                        (const unsigned int &f,
-                         const math::tuple<fdim,typename euclidian::space<Q,d>::vector> &l)
-                    {
-                        math::tuple<fdim,typename euclidian::space<Q,d>::vector> r;
-
-                        for (int i = 0; i < fdim; i++)
-                        {
-                            r.data[i] = apply (f, l.data[i]);
-                        }
-
-                        return r;
-                    }
             };
         };
     };
