107a0e505730593f58a9116ab11341a9eee08d01
Magnus Achim Deininger <magnus@ef.gy>
2013-11-07 13:04:12 +0000

    add documentation and test cases to the 'exponential' namespace

--- include/ef.gy/exponential.h
+++ include/ef.gy/exponential.h
@@ -1,4 +1,11 @@
 /**\file
+ * \brief Raise numbers to powers
+ *
+ * Calculations expressions of the form x^y, i.e. raises numbers to specified
+ * powers. Unlike the std::pow() function, these powers are specified at
+ * compile time, thus hopefully increasing runtime performance compared to
+ * generic algorithms - though quite probably still doing worse than optimised
+ * processor instructions for the job.
  *
  * \copyright
  * Copyright (c) 2012-2013, ef.gy Project Members
@@ -32,32 +39,127 @@ namespace efgy
 {
     namespace math
     {
+        /**\brief Templates to calculate exponential values
+         *
+         * The templates in this namespace can be used to raise numbers to
+         * specified powers.
+         */
         namespace exponential
         {
-            template<typename Q, int exponent>
+            /**\brief Integral, compile-time constant power
+             *
+             * This class defines a method raise() that raises a given number
+             * to the power given in the exponent template parameter.
+             *
+             * We're using a class template as opposed to a function template
+             * to allow for partial specialisation of the fix points.
+             *
+             * \tparam Q        Basic arithmetic type for calculations.
+             * \tparam exponent The exponent to raise numbers to with the
+             *                  raise() function.
+             */
+            template<typename Q, long exponent>
             class integral
             {
                 public:
+                    /**\brief Raise base to power
+                     *
+                     * Calculates base^exponent, i.e. calculates the
+                     * exponent'th power of the base argument. This exponent is
+                     * specified as a template argument.
+                     *
+                     * This function implements the square-and-multiply
+                     * algorithm to minimise the number of multiplications
+                     * needed to calculate the result.
+                     *
+                     * \param[in] base The number to raise.
+                     *
+                     * \return Base raised to the exponent'th power.
+                     */
                     static Q raise (const Q &base)
                     {
-                        return base * integral<Q,exponent-1>::raise(base);
+                        return (exponent % 2 == 0)
+                             ?         integral<Q, (exponent    >> 1)>::raise(base*base)
+                             : (base * integral<Q,((exponent-1) >> 1)>::raise(base*base));
                     }
             };
 
+            /**\brief Integral, compile-time constant power
+             *
+             * This is one of the fix points of the integral template. This one
+             * handles the case when the exponent is one, i.e. the raise()
+             * method simply returns the number without changing it.
+             *
+             * \tparam Q        Basic arithmetic type for calculations.
+             */
             template<typename Q>
             class integral<Q,1>
             {
                 public:
+                    /**\brief Return value unmodified
+                     *
+                     * Calculates base^1 = base, i.e. the return value is
+                     * always the number you pass to this function.
+                     *
+                     * \param[in] base The number to raise.
+                     *
+                     * \return The value of the base argument is passed through
+                     *         to the caller.
+                     */
                     static Q raise (const Q &base)
                     {
                         return base;
                     }
             };
 
+            /**\brief Integral, compile-time constant power
+             *
+             * This is one of the fix points of the integral template. This one
+             * handles the case when the exponent is negative one, i.e. the
+             * raise() method simply returns the reciprocal of its argument.
+             *
+             * \tparam Q        Basic arithmetic type for calculations.
+             */
+            template<typename Q>
+            class integral<Q,-1>
+            {
+                public:
+                    /**\brief Return reciprocal of value
+                     *
+                     * Calculates base^(-1) = 1/base, i.e. the return value is
+                     * the reciprocal of the argument.
+                     *
+                     * \param[in] base The number to raise.
+                     *
+                     * \return Returns the reciprocal of the number passed in.
+                     */
+                    static Q raise (const Q &base)
+                    {
+                        return Q(1)/base;
+                    }
+            };
+
+            /**\brief Integral, compile-time constant power
+             *
+             * This is one of the fix points of the integral template. This one
+             * handles the case when the exponent is zero, i.e. the raise()
+             * method simply discards its argument and returns one.
+             *
+             * \tparam Q        Basic arithmetic type for calculations.
+             */
             template<typename Q>
             class integral<Q,0>
             {
                 public:
+                    /**\brief Raise to the zeroth power
+                     *
+                     * Discards its argument and returns one, as one is the
+                     * zeroth power of any number.
+                     *
+                     * \param[in] base The number to raise.
+                     *
+                     * \return Always Q(1).
+                     */
                     static Q raise (const Q &base)
                     {
                         return Q(1);
