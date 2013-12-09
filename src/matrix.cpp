d0ccf2338a17c6c18981d07079421549de482e1c
Magnus Achim Deininger <magnus@ef.gy>
2013-12-09 10:18:55 +0000

    started documenting the matrix demo

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -45,124 +45,163 @@
 using namespace efgy;
 using namespace std::chrono;
 
-terminal::vt100<> output;
-random::mersenneTwister<> rng(1337);
-system_clock::time_point now;
-
-class matrixStream
+/**\brief Data and functions of the matrix example programme
+ *
+ * Contains global variables and classes used by the 'matrix' demo animation.
+ * This is a separate namespace to keep things all neat, clean and tidy.
+ */
+namespace thematrix
 {
-    public:
-        class cell
-        {
-            public:
-                cell(const unsigned long &pCharacter)
-                    : character(pCharacter), created(now) {}
+    /**\brief VT100 output buffer
+     *
+     * Encapsulates stdio to automatically generate the terminal escape
+     * sequences to write things at specific positions with specific colours.
+     */
+    terminal::vt100<> output;
 
-                unsigned long character;
-                system_clock::time_point created;
-        };
+    /**\brief Random number generator
+     *
+     * A seeded instance of the mersenne twister RNG; used to position the
+     * streams and to add and remove glyphs randomly.
+     */
+    random::mersenneTwister<> rng(1337);
 
-        matrixStream(const std::size_t &pLine, const std::size_t &pColumn)
-            : line(pLine), column(pColumn), last(now), doDelete(false) {}
+    /**\brief Current time
+     *
+     * The point in time when processing of the current streams started; used
+     * to determine when streams should be updated.
+     */
+    system_clock::time_point now;
 
-        std::size_t line;
-        std::size_t column;
-        std::vector<cell> data;
-        system_clock::time_point last;
-        bool doDelete;
-
-        bool update (void)
-        {
-            if ((now - last) > std::chrono::milliseconds(10))
+    /**\brief A stream of data
+     *
+     * A 'stream' is what I dubbed the individual columns of text in the
+     * animation. These are generated randomly and they mutate randomly as
+     * well.
+     */
+    class stream
+    {
+        public:
+            /**\brief A single glyph in a stream
+             *
+             * To create the animation, each of the glyphs in the output needs
+             * to be tagges with the time it was created, in order to know
+             * which colour to draw the glyph in.
+             */
+            class cell
             {
-                last = now;
-                switch (rng() % 3)
-                {
-                    case 0:
-                        if (data.size() > 0)
-                        {
-                            data.erase(data.begin() + (rng() % data.size()));
-                            break;
-                        }
-                    default:
-                        data.push_back(cell(rng() % (1 << 7)));
-                }
-            }
-            std::array<std::size_t,2> s = output.size();
-            if (data.size() > (s[1]/2))
+                public:
+                    cell(const unsigned long &pCharacter)
+                        : character(pCharacter), created(now) {}
+
+                    unsigned long character;
+                    system_clock::time_point created;
+            };
+
+            stream(const std::size_t &pLine, const std::size_t &pColumn)
+                : line(pLine), column(pColumn), last(now), doDelete(false) {}
+
+            std::size_t line;
+            std::size_t column;
+            std::vector<cell> data;
+            system_clock::time_point last;
+            bool doDelete;
+
+            bool update (void)
             {
-                if (line < s[1])
+                if ((now - last) > std::chrono::milliseconds(10))
                 {
-                    if (rng() % 5 == 0)
+                    last = now;
+                    switch (rng() % 3)
                     {
-                        output.target[line][column].content = ' ';
-                        line++;
+                        case 0:
+                            if (data.size() > 0)
+                            {
+                                data.erase(data.begin() + (rng() % data.size()));
+                                break;
+                            }
+                        default:
+                            data.push_back(cell(rng() % (1 << 7)));
                     }
                 }
-                else
+                std::array<std::size_t,2> s = output.size();
+                if (data.size() > (s[1]/2))
                 {
-                    doDelete = true;
+                    if (line < s[1])
+                    {
+                        if (rng() % 5 == 0)
+                        {
+                            output.target[line][column].content = ' ';
+                            line++;
+                        }
+                    }
+                    else
+                    {
+                        doDelete = true;
+                    }
+                    data.erase(data.begin());
                 }
-                data.erase(data.begin());
-            }
-            int i = line;
-            for (cell &d : data)
-            {
-                if (i >= s[1])
+                int i = line;
+                for (cell &d : data)
                 {
-                    break;
+                    if (i >= s[1])
+                    {
+                        break;
+                    }
+                    output.target[i][column].content = d.character;
+                    output.target[i][column].foregroundColour = (now - d.created) > std::chrono::milliseconds(120) ? 2 : 7;
+                    output.target[i][column].backgroundColour = 0;
+                    i++;
                 }
-                output.target[i][column].content = d.character;
-                output.target[i][column].foregroundColour = (now - d.created) > std::chrono::milliseconds(120) ? 2 : 7;
-                output.target[i][column].backgroundColour = 0;
-                i++;
+                return true;
             }
-            return true;
-        }
+    };
+
+    void handle_interrupt(int signal)
+    {
+        exit(1);
+    }
+
+    terminal::cell<long> postProcess
+        (const terminal::terminal<long>&t,
+         const std::size_t &l,
+         const std::size_t &c)
+    {
+        terminal::cell<long> rv = t.target[l][c];
+        rv.content = rv.content == 0 ? ' ' : rv.content;
+        rv.foregroundColour = rv.foregroundColour == 7 ? 7 : 2;
+        rv.backgroundColour = 0;
+        return rv;
+    }
+
+    terminal::cell<long> postProcessPolar
+        (const terminal::terminal<long>&t,
+         const std::size_t &pl,
+         const std::size_t &pc)
+    {
+        double l = pl;
+        double c = pc;
+        std::array<std::size_t,2> s = t.size();
+        double hl = s[1]/2;
+        double hc = s[0]/2;
+        double loff = l - hl;
+        double coff = c - hc;
+        double r2 = loff*loff + coff*coff;
+        double r = std::sqrt(r2);
+        l = hl + loff + std::sin(r) * 1.0;
+        c = hc + coff + std::cos(r) * 1.0;
+        const std::size_t tl = (std::size_t)l < s[1] ? (std::size_t)l : (s[1] - 1);
+        const std::size_t tc = (std::size_t)c < s[0] ? (std::size_t)c : (s[0] - 1);
+        terminal::cell<long> rv = t.target[tl][tc];
+        terminal::cell<long> cv = t.current[tl][tc];
+        rv.content = rv.content == 0 ? (cv.content == 0 ? ' ' : cv.content) : rv.content;
+        rv.foregroundColour = rv.foregroundColour == 7 ? 7 : 2;
+        rv.backgroundColour = 0;
+        return rv;
+    }
 };
 
-void handle_interrupt(int signal)
-{
-    exit(1);
-}
-
-terminal::cell<long> postProcess
-    (const terminal::terminal<long>&t,
-     const std::size_t &l,
-     const std::size_t &c)
-{
-    terminal::cell<long> rv = t.target[l][c];
-    rv.content = rv.content == 0 ? ' ' : rv.content;
-    rv.foregroundColour = rv.foregroundColour == 7 ? 7 : 2;
-    rv.backgroundColour = 0;
-    return rv;
-}
-
-terminal::cell<long> postProcessPolar
-    (const terminal::terminal<long>&t,
-     const std::size_t &pl,
-     const std::size_t &pc)
-{
-    double l = pl;
-    double c = pc;
-    std::array<std::size_t,2> s = t.size();
-    double hl = s[1]/2;
-    double hc = s[0]/2;
-    double loff = l - hl;
-    double coff = c - hc;
-    double r2 = loff*loff + coff*coff;
-    double r = std::sqrt(r2);
-    l = hl + loff + std::sin(r) * 1.0;
-    c = hc + coff + std::cos(r) * 1.0;
-    const std::size_t tl = (std::size_t)l < s[1] ? (std::size_t)l : (s[1] - 1);
-    const std::size_t tc = (std::size_t)c < s[0] ? (std::size_t)c : (s[0] - 1);
-    terminal::cell<long> rv = t.target[tl][tc];
-    terminal::cell<long> cv = t.current[tl][tc];
-    rv.content = rv.content == 0 ? (cv.content == 0 ? ' ' : cv.content) : rv.content;
-    rv.foregroundColour = rv.foregroundColour == 7 ? 7 : 2;
-    rv.backgroundColour = 0;
-    return rv;
-}
+using namespace thematrix;
 
 int main (int argc, char **argv)
 {
@@ -171,7 +210,7 @@ int main (int argc, char **argv)
 
     std::size_t i;
 
-    std::vector<matrixStream> streams;
+    std::vector<stream> streams;
 
     std::signal(SIGINT, handle_interrupt);
 
@@ -184,10 +223,10 @@ int main (int argc, char **argv)
         {
             std::size_t l = rng() % (s[1] / 3);
             std::size_t c = rng() %  s[0];
-            streams.push_back(matrixStream(l, c));
+            streams.push_back(stream(l, c));
         }
 
-        for (matrixStream &s : streams)
+        for (stream &s : streams)
         {
             s.update();
         }
