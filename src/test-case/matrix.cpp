01e141c099102ff27b021c26986b6ed0567a6bea
Nadja Deininger <nadja@ef.gy>
2014-01-05 02:53:25 +0100

    added tests for matrix assignment and addition

--- src/test-case/matrix.cpp
+++ src/test-case/matrix.cpp
@@ -64,4 +64,154 @@ int testConstruction (std::ostream &log)
      return 0;
 }
 
-TEST_BATCH(testConstruction)
+int testAssignment (std::ostream &log)
+{
+     matrix<int, 3, 4> m;
+     for(int i = 0; i < 3; i++)
+     {
+    	for(int k = 0; k < 4; k++)
+        {
+	    	m[i][k] = i * k;
+	    }
+     }
+
+     matrix<int, 3, 4> m2 = m;
+
+     bool allCorrect = true;
+     for(int i = 0; i < 3; i++)
+     {
+         for(int k = 0; k < 3; k++)
+         {
+            allCorrect &= (m2[i][k] == m[i][k]);
+         }
+     }
+
+     if(!allCorrect)
+     {
+         log << "Values were not copied correctly during assignment.";
+         return next_integer();
+     }
+
+     return 0;
+}
+
+int testAddition (std::ostream &log)
+{
+     matrix<int, 3, 4> m;
+     for(int i = 0; i < 3; i++)
+     {
+    	for(int k = 0; k < 4; k++)
+        {
+	    	m[i][k] = i * k;
+	    }
+     }
+
+     matrix<int, 3, 4> n;
+
+     for(int i = 0; i < 3; i++)
+     {
+    	for(int k = 0; k < 4; k++)
+        {
+	    	n[i][k] = 0; 
+	    }
+     }
+
+     // results of left and right addition of
+     // neutral element to m 
+     matrix<int, 3, 4> leftNeutral = n + m;
+     matrix<int, 3, 4> rightNeutral = m + n;
+
+     // test addition of neutral element;
+     bool allCorrectLeft = true;
+     bool allCorrectRight = true; 
+     for(int i = 0; i < 3; i++)
+     {
+         for(int k = 0; k < 4; k++)
+         {
+            allCorrectLeft &= (leftNeutral[i][k] == (i * k));
+            allCorrectRight &= (rightNeutral[i][k] == (i * k));
+         }
+     }
+     if(!allCorrectLeft) 
+     {
+        log << "n + m, where n is the neutral element: ";
+        for(int i = 0; i < 3; i++)
+        {
+            for(int k = 0; k < 4; k++)
+            {
+                log << leftNeutral[i][k];
+            }
+            log << "\n";
+        }
+        log << "\n";
+        log << "m (original matrix):";
+        for(int i = 0; i < 3; i++)
+        {
+            for(int k = 0; k < 4; k++)
+            {
+                log << m[i][k];
+            }
+            log << "\n";
+        }
+
+        log << "Unexpected result of left addition of neutral element to matrix.";
+        return next_integer();
+     }
+     if(!allCorrectRight)
+     {
+        log << "m + n, where n is the neutral element: ";
+        for(int i = 0; i < 3; i++)
+        {
+            for(int k = 0; k < 4; k++)
+            {
+                log << rightNeutral[i][k];
+            }
+            log << "\n";
+        }
+        
+        log << "\n";
+        log << "m (original matrix):";
+        for(int i = 0; i < 3; i++)
+        {
+            for(int k = 0; k < 4; k++)
+            {
+                log << m[i][k];
+            }
+            log << "\n";
+        }
+        log << "Unexpected result of right addition of neutral element to matrix.";
+        return next_integer();    
+     }
+
+     // another test for addition of two non-neutral elements
+     matrix<int, 3, 4> m2;
+
+     for(int i = 0; i < 3; i++)
+     {
+         for(int k = 0; k < 4; k++)
+         {
+             m2[i][k] = i + k;
+         }
+     }
+
+     matrix<int, 3, 4> sum = m + m2;
+     
+     bool allCorrectSum = true;
+     for(int i = 0; i < 3; i++)
+     {
+         for(int k = 0; k < 3; k++)
+         {
+             allCorrectSum &= (sum[i][k] == (i * k) + (i + k));
+         }
+     }
+
+     if(!allCorrectSum)
+     {
+         log << "Unexpected result adding two matrices.";
+         return next_integer();
+     }
+
+     return 0;
+}
+
+TEST_BATCH(testConstruction, testAssignment, testAddition)
