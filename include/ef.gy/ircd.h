f134607dad49a26a66e57837673f12fe7f3ccf77
Magnus Deininger <magnus@ef.gy>
2015-09-14 18:52:02 -0400

    fix up the help text for the IRC daemon bindings.

--- include/ef.gy/ircd.h
+++ include/ef.gy/ircd.h
@@ -46,13 +46,20 @@ static std::size_t setup(net::endpoint<sock> lookup,
   });
 }
 
-static cli::option socket("irc:unix:(.+)", [](std::smatch &m) -> bool {
-  return setup(net::endpoint<asio::local::stream_protocol>(m[1])) > 0;
-});
+static cli::option
+    socket("-{0,2}irc:unix:(.+)",
+           [](std::smatch &m) -> bool {
+             return setup(net::endpoint<asio::local::stream_protocol>(m[1])) >
+                    0;
+           },
+           "Listen for IRC connections on the given unix socket[1].");
 
-static cli::option tcp("irc:(.+):([0-9]+)", [](std::smatch &m) -> bool {
-  return setup(net::endpoint<asio::ip::tcp>(m[1], m[2])) > 0;
-});
+static cli::option
+    tcp("-{0,2}irc:(.+):([0-9]+)",
+        [](std::smatch &m) -> bool {
+          return setup(net::endpoint<asio::ip::tcp>(m[1], m[2])) > 0;
+        },
+        "Listen for IRC connections on the given host[1] and port[2].");
 }
 }
 
