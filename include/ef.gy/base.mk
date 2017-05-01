5f50cc57d51327e3e55c97b2eadcce677daabde5
Magnus Deininger <magnus@ef.gy>
2017-05-01 17:03:04 +0000

    remove the CC makefile variable (no plain C would be using this C++ library); search for clang++ with version name, as we want the most recent compiler installed and Debian/Ubuntu seems to be notoriously bad at setting things up.

--- include/ef.gy/base.mk
+++ include/ef.gy/base.mk
@@ -15,12 +15,11 @@ VERSION:=1
 STYLE:=Google
 
 # standard programmes (may or may not be used)
-CC:=$(shell which clang false | head -n 1)
 CLANG_FORMAT:=$(shell which clang-format clang-format-3.5 clang-format-3.4 false | head -n 1) -style=$(STYLE)
 CLDOC:=$(shell which cldoc false | head -n 1)
 CSSMIN:=$(shell which cssmin false | head -n 1)
 CURL:=$(shell which curl false | head -n 1) -s
-CXX:=$(shell which clang++ false | head -n 1)
+CXX:=$(shell which clang++-3.9 clang++-3.8 clang++-3.7 clang++-3.6 clang++-3.5 clang++ g++ c++ false | head -n 1)
 EMXX:=$(shell which em++ false | head -n 1)
 GIT:=$(shell which git false | head -n 1)
 INSTALL:=$(shell which install false | head -n 1)
